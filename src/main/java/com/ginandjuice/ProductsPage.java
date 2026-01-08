9bb3b1c2b4dc58f18ae51fc7443b633d6d6bbb2e
marikadu <marika1705@bk.ru>
2026-01-08 16:05:19 +0200

    Place every product in the cart

--- src/main/java/com/ginandjuice/ProductsPage.java
+++ src/main/java/com/ginandjuice/ProductsPage.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class ProductsPage extends HomePage{
     private By productsPageTitle = By.xpath("//h1[text()='Products']");
     private By productDetailsButton = By.xpath("//section[@class='maincontainer']//span[@class='button']");
+    int amount;
+
+    private By addToCartButton = By.xpath("//form[@id='addToCartForm']//button[@class='button']");
 
     // 1. Find the Number of products
     // 2. Create a loop through all the products to place all of them into the shopping card
@@ -18,7 +21,18 @@ public class ProductsPage extends HomePage{
 
     public void getNumberOfProducts(){
         List<WebElement> productButtons = driver.findElements(productDetailsButton);
-        int amount = productButtons.size();
-        System.out.println("Number of products:" + amount);
+        amount = productButtons.size();
+        System.out.println("Number of products: " + amount);
+    }
+
+    public void goToProductPageAndAddToCart(){
+        var i = 1;
+        while (i <= amount){
+            driver.get("https://ginandjuice.shop/catalog/product?productId=" + i);
+            scrollToElementJS(addToCartButton);
+            click(addToCartButton);
+            i++;
+        }
+
     }
 }
--- src/test/java/test/OrderTest.java
+++ src/test/java/test/OrderTest.java
@@ -7,8 +7,8 @@ import org.testng.annotations.Test;
 public class OrderTest extends BaseTest {
     String coupon;
 
-    // 3. place an order
-    // 4. confirm order
+    // 3. Place an order
+    // 4. Confirm order
 
     @Test
     public void placingOrderWithCouponTest(){
@@ -17,15 +17,22 @@ public class OrderTest extends BaseTest {
         homePage.fillEmailField("mail@mail.com");
         homePage.clickSubscribeButton();
         coupon = homePage.getTheCoupon(); // Save the coupon
-        System.out.println("Found coupon:" + coupon);
+        System.out.println("Found coupon: " + coupon);
         homePage.clickCloseButton();
 
         // 2. Add products to the shopping cart
+        // 2.1 Get the number of all products
+
         var productsPage = homePage.goToProductsPage();
         String actualTitle = productsPage.getProductsPageTitle();
         String expectedTitle = "PRODUCTS";
         Assert.assertEquals(actualTitle,  expectedTitle, "\n Could not find the page title \n");
 
         productsPage.getNumberOfProducts(); // Find the number of products
+
+        // 2.2 Open every product page and add them to the cart
+        productsPage.goToProductPageAndAddToCart();
+
+        // 3. Go to cart page
     }
 }
