f422e45d36fcbcb1aaa10ddbb8f1d9e33f562e23
marikadu <marika1705@bk.ru>
2026-01-08 09:00:31 +0200

    Created the project

---
+++ src/main/java/com/ginandjuice/HomePage.java
@@ -0,0 +1,4 @@
+package com.ginandjuice;
+
+public class HomePage {
+}
---
+++ src/test/java/test/OrderTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class OrderTest extends BaseTest {
+    // 1. add things to shopping card
+    // 2. get the coupon
+    // 3. place an order
+    // 4. confirm order
+    @Test
+    public void placingOrderWithCouponTest(){
+
+    }
+}
