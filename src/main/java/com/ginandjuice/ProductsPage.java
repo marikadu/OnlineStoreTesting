a93e3065fdf5eff320f2dc1728c0ced39caaf563
marikadu <marika1705@bk.ru>
2026-01-09 08:01:02 +0200

    Taking screenshot of every product

--- src/main/java/com/ginandjuice/ProductsPage.java
+++ src/main/java/com/ginandjuice/ProductsPage.java
@@ -1,20 +1,29 @@
 package com.ginandjuice;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.io.FileHandler;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class ProductsPage extends HomePage{
     private By productsPageTitle = By.xpath("//h1[text()='Products']");
     private By productDetailsButton = By.xpath("//section[@class='maincontainer']//span[@class='button']");
-    int amount;
-
     private By addToCartButton = By.xpath("//form[@id='addToCartForm']//button[@class='button']");
-
     public String getProductsPageTitle(){
         return find(productsPageTitle).getText();
     }
+    private By productNamePlacement = By.xpath("//section[@class='product']//h3");
+
+    int amount;
+    private int productId = 1;
+    private String productNameWithSpaces;
+    private String productName;
+
 
     // 1. Find the Number of products
     public void getNumberOfProducts(){
@@ -25,13 +34,38 @@ public class ProductsPage extends HomePage{
 
     // 2. Create a loop through all the products to place all of them into the shopping card
     public void goToProductPageAndAddToCart(){
-        var i = 1;
-        while (i <= amount){
-            driver.get("https://ginandjuice.shop/catalog/product?productId=" + i);
+        while (productId <= amount){
+            driver.get("https://ginandjuice.shop/catalog/product?productId=" + productId);
+            getProductName();
+
+            takeScreenshot(productName); // Take screenshot of every product
+            // Additionally: calculate the price
             scrollToElementJS(addToCartButton);
             click(addToCartButton);
-            i++;
-            // Additionally: take screenshot of every product
+            productId++;
         }
+    }
+
+
+    private String getProductName(){
+        find(productNamePlacement);
+        productNameWithSpaces = getText(productNamePlacement);
+        productName = productNameWithSpaces.replaceAll("[ ]", "_");
+        return productName;
+    }
+
+
+    public void takeScreenshot(String productName){
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+        File destination = new File(System.getProperty("user.dir") + "/resources/screenshots/"
+                + java.time.LocalDate.now().toString() + "_productId_" + productId + "_" + productName + ".png");
+
+        try {
+            FileHandler.copy(source, destination);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Screenshot located at: " + destination);
     }
 }
